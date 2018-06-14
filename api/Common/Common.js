454acfd279dda989df5319f8fb6d4a020b4247aa
Krishna K <yasodakrishnak.mca@gmail.com>
2018-06-14 17:06:32 +0530

    add fs and formidable

---
+++ api/Common/Common.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var autoIncrement = require("mongodb-autoincrement");
+
+var MongoClient = require('mongodb').MongoClient;
+
+ var url = "mongodb://heroku_zn69xqhf:pplo9p5dcjqn6i3l0cdeiov71v@ds259250.mlab.com:59250/heroku_zn69xqhf";
