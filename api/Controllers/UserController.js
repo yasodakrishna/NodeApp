5f12fa7d395146ca8cd7313f1fbd388060dd4948
Krishna K <yasodakrishnak.mca@gmail.com>
2018-06-14 18:19:27 +0530

    removed file upload code

--- api/Controllers/UserController.js
+++ api/Controllers/UserController.js
@@ -3,10 +3,7 @@
 var autoIncrement = require("mongodb-autoincrement");
 
 var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://heroku_zn69xqhf:pplo9p5dcjqn6i3l0cdeiov71v@ds259250.mlab.com:59250/heroku_zn69xqhf";
-
-var formidable = require('formidable');
-var fs = require('fs'); 
+var url = "mongodb://heroku_zn69xqhf:pplo9p5dcjqn6i3l0cdeiov71v@ds259250.mlab.com:59250/heroku_zn69xqhf";
 
 // to get all the Buildings information
 exports.getUsers = function (req, res) {  
@@ -23,50 +20,30 @@ exports.getUsers = function (req, res) {
   };
 
   // User Registration
-  exports.signUp = function(req, res){
-    var input = req.body; 
-    console.log('input is:'); 
-    console.log(input);
-    
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-    var values = fields;
-    var oldpath = files.profilepic.path;
-    console.log(oldpath);
-    filenamechange(files.profilepic.name,function(newname){
-    var newpath = './Uploads' + newname;
-    console.log(newpath);
-    fs.rename(oldpath, newpath, function (err) {
-    if (err) throw err;
-    console.log("file uploaded");
-    })
-    MongoClient.connect(url, function(err, db){ 
-    var dbo = db.db("heroku_zn69xqhf");
-    var collectionName="CLC_User";
-    autoIncrement.getNextSequence(dbo, collectionName,"UserID", function (err, autoIndex) {
-    var query = { 
-    UserID:autoIndex,
-    Email : values.Email ,
-    Password : values.Password,
-    UserType: values.UserType,
-    UserProfileImage: newpath};
-    dbo.collection(collectionName).insertOne(query, function(err, result) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    res.json({status : 'success', message : 'OK', result : input});
-    db.close();
-    });
-    });
-    });
-    })
-    })
-    }
-    function filenamechange(oldname, callback){
-    var arr = oldname.split('.');
-    var extension = arr[arr.length-1];
-    var newname = 'profilepic_' + Date.now() + '.' + extension;
-    callback(newname);
-    }
+exports.signUp = function(req, res){
+  var input = req.body; 
+  console.log('input is:'); 
+  console.log(input);
+  
+          MongoClient.connect(url, function(err, db){ 
+              var dbo = db.db("heroku_zn69xqhf");
+              var collectionName="CLC_User";
+              autoIncrement.getNextSequence(dbo, collectionName,"UserID", function (err, autoIndex) {
+              var query = { 
+                  UserID:autoIndex,
+                  Email : input.Email ,
+                  Password : input.Password,
+                  UserType: input.UserType,
+                  UserProfileImage: input.UserProfileImage};
+              dbo.collection(collectionName).insertOne(query, function(err, result) {
+                  if (err) throw err;
+                  console.log("1 document inserted");
+                  res.json({status : 'success', message : 'OK', result : input});
+                  db.close();
+                });
+              });
+          });
+      }
 
       // Validate User && input.Password==result.Password 
 exports.UserValidate=function(req, res){
