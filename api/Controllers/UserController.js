fa2291ab89e638318499105faa182b4e6c86752c
Krishna K <yasodakrishnak.mca@gmail.com>
2018-06-14 10:59:03 +0530

    SINGUP added

--- api/Controllers/UserController.js
+++ api/Controllers/UserController.js
@@ -3,20 +3,6 @@
 var MongoClient = require('mongodb').MongoClient;
     var url = "mongodb://heroku_zn69xqhf:pplo9p5dcjqn6i3l0cdeiov71v@ds259250.mlab.com:59250/heroku_zn69xqhf";
 
-// var mongoose = require('mongoose'),
-   //UsersData = MongoClient.model('CLC_User');
-//   var db = mongoose.connection;
-//   db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function callback () {
-//   console.log("h");
-//   UsersData.find(function (err, _UsersData) {
-//     if (err)
-//     console.log("error");
-//     else {
-//       console.log(_UsersData);      
-//     }
-//   });
-// });
 // to get all the Buildings information
 exports.getUsers = function (req, res) {  
   MongoClient.connect(url, function(err, db){ 
@@ -25,9 +11,33 @@ exports.getUsers = function (req, res) {
   dbo.collection("CLC_User").findOne({}, function(err, result) {
     if (err) throw err;
     console.log(result.Email);
-    res.json({status : 'success', message : 'OK', result : result});
-    //res.json(result.Email);
+    res.json({status : 'success', message : 'OK', result : result});    
     db.close();
   });  
 });
   };
+
+  // User Registration
+exports.signUp = function(req, res){
+  var input = req.body; 
+  console.log('input is:'); 
+  console.log(input);
+          MongoClient.connect(url, function(err, db){ 
+              var dbo = db.db("heroku_zn69xqhf");
+              var collectionName="CLC_User";
+              autoIncrement.getNextSequence(dbo, collectionName,"UserID", function (err, autoIndex) {
+              var query = { 
+                  UserID:autoIndex,
+                  Email : input.Email ,
+                  Password : input.Password,
+                  UserType: input.UserType,
+                  UserProfileImage: input.UserProfileImage};
+              dbo.collection(collectionName).insertOne(query, function(err, result) {
+                  if (err) throw err;
+                  console.log("1 document inserted");
+                  res.json({status : 'success', message : 'OK', result : input});
+                  db.close();
+                });
+              });
+          });
+      }
--- server.js
+++ server.js
@@ -1,30 +1,9 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
-    mongoose = require('mongoose'),     
-    //http = require('http'),
     bodyParser = require('body-parser');
-   
-    var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://heroku_zn69xqhf:pplo9p5dcjqn6i3l0cdeiov71v@ds259250.mlab.com:59250/heroku_zn69xqhf";
-
-//mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://192.168.100.23/RMSDB');
-//var url1 = mongoose.connect("mongodb://krishnamongo:k9550793089@ds153460.mlab.com:53460/property")
-//mongoose.connect('mongodb://heroku_zn69xqhf:pplo9p5dcjqn6i3l0cdeiov71v@ds259250.mlab.com:59250/heroku_zn69xqhf');
-//'mongodb://heroku_r9hn0jzn:echn9ckdip4644i79p2j4blun8@ds129796.mlab.com:29796/heroku_r9hn0jzn');
-
-// MongoClient.connect(url, function(err, db){ 
-//     if (err) throw err;
-//   var dbo = db.db("property");
-//   dbo.collection("CLC_User").findOne({}, function(err, result) {
-//     if (err) throw err;
-//     console.log(result.Email);
-//     db.close();
-//   });  
-// });
-
-User = require('./api/Models/UserModel'),
+  
+//User = require('./api/Models/UserModel'),
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
