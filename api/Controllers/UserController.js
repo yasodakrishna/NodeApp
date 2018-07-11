ca132700c88786fed25483a4b6bca512300dcc88
Junaid Syed <yasodakrishnak.mca@gmail.com>
2018-07-11 15:52:52 +0530

    code updated

--- api/Controllers/UserController.js
+++ api/Controllers/UserController.js
@@ -48,7 +48,7 @@ exports.getUsers = function(req, res){
            console.log(oldpath);
            filenamechange(files.UserProfileImage.name,function(newname){
                console.log(newname);
-               var newpath ='./uploads/' + newname;
+               var newpath ='./ProfilePic/' + newname;
                console.log(newpath);
                fs.rename(oldpath, newpath, function (err) {
                    console.log(newpath);
@@ -79,13 +79,14 @@ exports.getUsers = function(req, res){
          })
     }
 
-function filenamechange(oldname, callback){
-    var arr = oldname.split('.');
-    var extension = arr[arr.length-1];
-    var newname = 'ProfilePic_' + Date.now() + '.' + extension;
-    callback(newname);
-}
-
+    function filenamechange(oldname, callback){
+        v//ar arr = oldname.split('.');
+        var type=oldname.type;
+        var ext = type.split("/");
+        var newname =  "profile_"+ Date.now() +'.'+ ext[1];
+        callback(newname);
+    }
+    
     exports.UserValidate=function(req, res){
         var input = req.body;
         signupValidation(input, function(errMessage){
